c05776feac6daf8e021fbd73e41ca0d9efcf3e07
uaalumni02 <dbell1911@gmail.com>
2024-06-02 11:24:08 -0500

    cookies added

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import mongoose, { connect } from "mongoose";
+
+const app = express();
+const { log, error } = console;
+
+const port = process.env.PORT || 3000;
+
+const router = express.Router();
+
+import userRoutes from "./routes/user.routes";
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const DB_URL = process.env.MONGO_URL;
+
+try {
+  mongoose.connect(DB_URL, { useNewUrlParser: true });
+  console.log("Connection Successful");
+} catch (err) {
+  console.error("Unable to Connect to MongoDB", err);
+}
+
+router.use("/user", userRoutes);
+
+app.use("/api", router);
+
+app.listen(port, () => log("server is running"));
+export default app;
